569d390832d1a2fab7a3fb4004fbc62c69bb8f95
刘克付 <liukefu2020@sina.com>
2017-10-22 08:58:56 +0800

    example update

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -2,11 +2,12 @@ package com.example;
 
 import android.app.Application;
 
-import com.facebook.react.ReactApplication;
 import com.brentvatne.react.ReactVideoPackage;
+import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
 import com.mg.app.PickerPackage;
 
 import java.util.Arrays;
@@ -16,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
         @Override
-        protected boolean getUseDeveloperSupport() {
+        public boolean getUseDeveloperSupport() {
             return BuildConfig.DEBUG;
         }
 
@@ -28,10 +29,21 @@ public class MainApplication extends Application implements ReactApplication {
                     new PickerPackage()
             );
         }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
     };
 
     @Override
     public ReactNativeHost getReactNativeHost() {
         return mReactNativeHost;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
 }
