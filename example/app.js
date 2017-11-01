139cf959a6a68e393459e01b6f6b4ea42d8b9b40
刘克付 <liukefu2020@sina.com>
2017-11-01 18:33:40 +0800

    update

--- example/app.js
+++ example/app.js
@@ -6,7 +6,7 @@ import {
 
 import Video from 'react-native-video';
 
-var ImagePicker = NativeModules.ImageCropPicker;
+import ImagePicker from 'react-native-customized-image-picker';
 
 const styles = StyleSheet.create({
   container: {
